388d349e3e1120c2c0311f3043910246afa7735f
Awen <cesar.santos23@estudiante.uva.es>
2024-12-04 17:46:39 +0100

    Im just a Raul commiting

--- src/src/Pricipal/MenuCoches.java
+++ src/src/Pricipal/MenuCoches.java
@@ -2,13 +2,9 @@ package Pricipal;
 
 import EntradaSalida.MyInput;
 
+public class MenuCoches extends MenuPrincipal{
 
-
-public class MenuPrincipal {
-
-    public static void principal()
-
-    {
+    public static void principal(){
         int opcion;
         boolean salir = true;
         while (salir) {
@@ -16,37 +12,37 @@ public class MenuPrincipal {
             opcion = elegir_opcion();
             switch (opcion) {
                 case 0:
-                    System.out.println("En proceso");
                     salir = false;
                     break;
                 case 1:
                     System.out.println("En proceso");
                     break;
                 case 2:
-                    MenuUsuarios.principal();
+                    System.out.println("En proceso");
                     break;
                 case 3:
-                   MenuVentas.principal();
+                    System.out.println("En proceso");
+                    break;
+                case 4:
+                    System.out.println("En proceso");
                     break;
                 default:
                     System.out.println("Opcion no Correcta");
-
             }
         }
     }
 
-
     public static void mostrar_opciones(){
-        System.out.println("Opciones del Concesionario");
-        System.out.println("--------------------------");
-        System.out.println("0. Salir del Concesionario");
-        System.out.println("1. Gestion de Ventas ");
-        System.out.println("2. Gestion de Cliente");
-        System.out.println("3. Gestion de Producto");
+        System.out.println("Menu de coches");
+        System.out.println("-----------------");
+        System.out.println("0. Salir del menu de usuarios");
+        System.out.println("1. Alta de coches");
+        System.out.println("2. Mostrar detalles de un coche");
+        System.out.println("3. Aumentar el stock");
+        System.out.println("4. Mostrar todos los coches de una seccion");
     }
-
     public static int elegir_opcion(){
         return MyInput.readInt();
     }
-
 }
+
---
+++ src/src/Pricipal/MenuVentas.java
@@ -0,0 +1,47 @@
+package Pricipal;
+
+import EntradaSalida.MyInput;
+import GestionableConcesionario.GestionUsuarios;
+
+public class MenuVentas extends MenuPrincipal{
+
+        public static void principal(){
+            int opcion;
+            boolean salir = true;
+            while (salir) {
+                mostrar_opciones();
+                opcion = elegir_opcion();
+                switch (opcion) {
+                    case 0:
+                        salir = false;
+                        break;
+                    case 1:
+                        System.out.println("En proceso");
+                        break;
+                    case 2:
+                        System.out.println("En proceso");
+                        break;
+                    case 3:
+                        System.out.println("En proceso");
+                        break;
+                    case 4:
+                        System.out.println("En proceso");
+                        break;
+                    default:
+                        System.out.println("Opcion no Correcta");
+                }
+            }
+        }
+
+        public static void mostrar_opciones(){
+            System.out.println("Menu de Ventas");
+            System.out.println("-----------------");
+            System.out.println("0. Salir del menu de usuarios");
+            System.out.println("1. Registrar una venta");
+            System.out.println("2. Mostrar informacion de una venta a base de su id");
+            System.out.println("3. Mostrar informacion de todas las ventas de un cliente");
+        }
+        public static int elegir_opcion(){
+            return MyInput.readInt();
+        }
+}
